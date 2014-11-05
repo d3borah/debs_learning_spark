4df08d211c71405680bba5261d1c943b04e7ca5f
Deborah Siegel <deborah.siegel@gmail.com>
2014-11-05 12:43:16 -0800

    Update WordCount.scala

--- WordCount.scala
+++ WordCount.scala
@@ -3,9 +3,10 @@
 val inputfile = sc.textFile("README.md")  
 
 /*
-each partition generates a list of keywords, so its a list of lists. what we really want is a list of keywords. 
-flatMap works applying a map function that returns a sequence for each element in the list, and then flattening the results 
-interesting resource with other flatmap examples (not Spark specific):
+each partition generates a list of keywords, so its a list of lists. what we really want is a 
+list of keywords. flatMap works applying a map function that returns a sequence for each element 
+in the list, and then flattening the results interesting resource with other flatmap examples 
+(not Spark specific):
 http://alvinalexander.com/scala/collection-scala-flatmap-examples-map-flatten
 */
 
@@ -13,6 +14,9 @@ val keywords = inputfile.flatMap(l => l.split(" "))
 val myMap = keywords.map(word => (word,1))
 val myReduce = myMap.reduceByKey(_ + _)
 
-//with saveAsTextFile you will get some partition files back. would need post-processing to force it into one file. or consider Apache Avro.
+/*
+with saveAsTextFile you will get some partition files back. would need post-processing to force 
+it into one file. or consider Apache Avro.
+*/
 
 myReduce.saveAsTextFile("wc_out")
