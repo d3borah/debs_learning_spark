b37c27c7767ee0d48c243a89da39045411ceff8d
Deborah Siegel <deborah.siegel@gmail.com>
2014-11-05 12:42:33 -0800

    Update WordCount.scala

--- WordCount.scala
+++ WordCount.scala
@@ -1,16 +1,18 @@
-#base RDD from text file - each element of the RDD is a line
+//base RDD from text file - each element of the RDD is a line
 
 val inputfile = sc.textFile("README.md")  
 
-#each partition generates a list of keywords, so its a list of lists. what we really want is a list of keywords. 
-#flatMap works applying a map function that returns a sequence for each element in the list, and then flattening the results 
-# interesting resource with other flatmap examples (not Spark specific):
-# http://alvinalexander.com/scala/collection-scala-flatmap-examples-map-flatten
+/*
+each partition generates a list of keywords, so its a list of lists. what we really want is a list of keywords. 
+flatMap works applying a map function that returns a sequence for each element in the list, and then flattening the results 
+interesting resource with other flatmap examples (not Spark specific):
+http://alvinalexander.com/scala/collection-scala-flatmap-examples-map-flatten
+*/
 
 val keywords = inputfile.flatMap(l => l.split(" "))
 val myMap = keywords.map(word => (word,1))
 val myReduce = myMap.reduceByKey(_ + _)
 
-#with saveAsTextFile you will get some partition files back. would need post-processing to force it into one file. or consider Apache Avro.
+//with saveAsTextFile you will get some partition files back. would need post-processing to force it into one file. or consider Apache Avro.
 
 myReduce.saveAsTextFile("wc_out")
