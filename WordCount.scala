8b600769fa30235edc2ad8a6dfdcd637888d6c6a
Deborah Siegel <deborah.siegel@gmail.com>
2014-11-06 12:30:13 -0800

    Update WordCount.scala

--- WordCount.scala
+++ WordCount.scala
@@ -2,8 +2,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 val conf = new SparkConf().setMaster("local").setAppName("My App")
-val sc = new SparkContext("local", "My App")
-
+//val sc = new SparkContext("local", "My App")
+val sc = new SparkContext(conf)
 
 //BASE RDD (HADOOP RDD) from text file. Each element of the RDD is a line.
 
